7cbe3453e7aa512faef5956b02163184b2929dbd
GUILHERME <GUILHERME @guilherme>
2026-02-13 19:31:54 -0300

    COMMIT 2

--- src/javaStudy/maiorNumero.java
+++ src/javaStudy/maiorNumero.java
@@ -1,5 +1,36 @@
 package javaStudy;
 
+import java.util.Scanner;
+
 public class maiorNumero {
+	public static void main(String[] args) 
+	{
+	
+		Scanner Entrada = new Scanner(System.in);
+		
+		System.out.println("digite o primeiro número:");
+		int n1 = Entrada.nextInt();
+		
+		  System.out.println("digite o segundo número:");
+		  int n2 = Entrada.nextInt();
+		  
+			System.out.println("digite o terceiro número:");
+			int n3 = Entrada.nextInt();
+			
+			 int maior = n1;
+			
+			 if ( n2 > maior ) { n2 = maior;	
+			
+			 } if ( n3 > maior ) { n3 = maior;
+			
+			  } if ( n1 == n2 && n2 == n3 ) { System.out.println("Todos números são iguais!");
+				
+			   } else { System.out.println("");
+			   
+			   Entrada.close();
+				
+			}
+					
+	  }
 
 }
